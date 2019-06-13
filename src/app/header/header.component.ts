602c76fde87d16e3bea0dfab1452e11c8a1ae42e
Tasfeq <tasfeq123@gmail.com>
2019-06-13 03:43:45 +0200

    feed update logic added and UI fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  private updatePossible:boolean = false;
+  private showSpinner:boolean = false;
+  private alertMessage:string = "Update Not Possible. Wait few more minutes"
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    console.log(this.updatePossible);
+  }
+
+  updateCheck(){
+    this.http
+    .get<boolean>("http://localhost:8080/update-feeds")
+    .subscribe(result =>{
+      console.log(result);
+      if(result){
+        this.updateFeeds();
+        this.updatePossible=false;
+        this.showSpinner=true;
+      }else{
+        this.updatePossible = true;
+        this.showSpinner=false;
+      }
+    })
+  };
+
+  updateFeeds(){
+    this.http.get("http://localhost/FeedCrawler-Server/");
+    setTimeout(() =>{
+       this.showSpinner=false;
+    },20000);
   }
 
 }
